630ef05822e8b808a2e2920c1b777af27bb25704
CarlosGo1994 <charls21ronald@gmail.com>
2020-06-02 19:41:38 -0500

    Suma method with generics

---
+++ src/com/excercise23generics/com/GenericsApp.java
@@ -0,0 +1,35 @@
+package com.excercise23generics.com;
+
+
+import com.excercise23generics.model.Suma;
+import com.excercise23generics.model.Resta;
+import com.excercise23generics.model.Multi;
+
+
+
+public class GenericsApp {
+
+	public static void main(String[] args) 
+	{
+		Suma<Integer> miObjeto = new Suma<Integer>(15);
+		Suma<String> miObjetoString = new Suma<String>("Testo");
+		Resta<Integer, Integer> miObjetoResta = new Resta<Integer, Integer>(10, 20);
+		Resta<Integer, Integer> miObjetoResta2 = new Resta<Integer, Integer>();
+		
+		
+		System.out.println("Objeto de miObjeto:"+miObjeto.getMiObjeto());
+		System.out.println("Objeto de miObjetoString:"+miObjetoString.getMiObjeto());
+		
+		miObjetoResta2.setKey(100);
+		miObjetoResta2.setValue(200);
+		System.out.println("Objeto Resta1:"+miObjetoResta.ToString());
+		System.out.println("Objeto Resta2:"+miObjetoResta2.ToString());
+		
+		System.out.println(Multi.MuestraTipoDato(10));
+		System.out.println(Multi.MuestraTipoDato("El Macho"));
+		System.out.println(Multi.MuestraTipoDato(10.0));
+		
+		System.out.println("10.0+10.0="+Suma.SumaNumeros(10.0, 10.0));
+	}
+
+}
